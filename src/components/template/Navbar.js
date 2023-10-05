4e78582a251be80f872923507af93f05a53e5468
zmhp <manashahp1995@gmail.com>
2023-10-06 00:13:26 +0530

    Hide Navbar top section on scroll

--- src/components/template/Navbar.js
+++ src/components/template/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { TfiEmail } from 'react-icons/tfi';
@@ -13,34 +13,57 @@ import { useRouter } from 'next/router';
 
 const Navbar = ({ navigation, onClickNavItem }) => {
 
+	const [scrolled, setScrolled] = useState(false);
+
+	useEffect(()=>{
+		const handleScroll = () => {
+			if (window.scrollY > 100) {
+				setScrolled(true);
+			} else {
+				setScrolled(false);
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	const router = useRouter();
 
 	return (
 		<>
 			<nav className='sticky top-0 z-50'>
-				<div className="flex flex-row justify-between items-center py-2 bg-primary-darker text-white font-normal px-8 text-base w-full">
-					<div className="flex">
-						<TfiEmail className="scale-1 mr-5" />
-						<span className="-mt-[6px]">
-							sample@erplanka.com
-							<span className="ml-5"> | </span>
-						</span>
-						<BsWhatsapp className="scale-1 mx-5" />
-						<Link
-							href="https://wa.me/+94777397487?text=More%20informations%20about%20ERP%20Lanka"
-							className="-mt-[6px]"
-						>
-							+94 77 739 7487
-						</Link>
+
+				{ !scrolled &&
+
+					<div className="flex flex-row justify-between items-center py-2 bg-primary-darker text-white font-normal px-8 text-base w-full transition-all duration-1000 ease-in-out">
+						<div className="flex">
+							<TfiEmail className="scale-1 mr-5" />
+							<span className="-mt-[6px]">
+								sample@erplanka.com
+								<span className="ml-5"> | </span>
+							</span>
+							<BsWhatsapp className="scale-1 mx-5" />
+							<Link
+								href="https://wa.me/+94777397487?text=More%20informations%20about%20ERP%20Lanka"
+								className="-mt-[6px]"
+							>
+								+94 77 739 7487
+							</Link>
+						</div>
+
+						<div className="flex">
+							<FaFacebookSquare className="scale-1 mr-5" />
+							<FaTwitterSquare className="scale-1 mr-5" />
+							<BsLinkedin className="scale-1 mr-5" />
+							<FaYoutubeSquare className="scale-1 mr-5" />
+						</div>
 					</div>
 
-					<div className="flex">
-						<FaFacebookSquare className="scale-1 mr-5" />
-						<FaTwitterSquare className="scale-1 mr-5" />
-						<BsLinkedin className="scale-1 mr-5" />
-						<FaYoutubeSquare className="scale-1 mr-5" />
-					</div>
-				</div>
+				}
 
 				<div className="flex flex-row justify-between items-center px-8 py-4 w- bg-white">
 					<Image
